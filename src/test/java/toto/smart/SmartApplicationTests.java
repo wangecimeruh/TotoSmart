837043b553f9dc4d3b5e577242f571d2037503b3
wangecimeruh <40371297+wangecimeruh@users.noreply.github.com>
2021-01-11 12:00:36 +0300

    first commit

---
+++ src/test/java/toto/smart/SmartApplicationTests.java
@@ -0,0 +1,13 @@
+package toto.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
